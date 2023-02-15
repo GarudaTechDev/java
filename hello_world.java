72b811b25386d7be662219b7e75882260fd4631a
Tech Garuda <81281735+GarudaTechDev@users.noreply.github.com>
2023-02-15 23:05:59 +0530

    Update file

--- hello_world.java
+++ hello_world.java
@@ -1,5 +1,1 @@
-class hello_world {
-    public static void main(String args[]){
-        System.out.println("Hello World");
-    }
-   }
+Y2xhc3MgaGVsbG9fd29ybGQgewogICAgcHVibGljIHN0YXRpYyB2b2lkIG1haW4oU3RyaW5nIGFyZ3NbXSl7CiAgICAgICAgU3lzdGVtLm91dC5wcmludGxuKCJIZWxsbyBXb3JsZCIpOwogICAgfQogICB9
